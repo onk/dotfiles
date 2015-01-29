3963b68daf32b015adf46ae4bff1ece05182d633
Takafumi ONAKA <takafumi.onaka@gmail.com>
2015-01-29 19:35:33 +0900

    [tig] add .tigrc

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ DOTFILES_DIR=$HOME/dotfiles
 ln -sf $DOTFILES_DIR/.zshrc $HOME/.zshrc
 ln -sf $DOTFILES_DIR/.zsh $HOME/.zsh
 ln -sf $DOTFILES_DIR/.gitconfig $HOME/.gitconfig
+ln -sf $DOTFILES_DIR/.tigrc $HOME/.tigrc
