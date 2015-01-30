938e9d0c5a77bfd10c9f541bd5e11370b3bb15b2
Takafumi ONAKA <takafumi.onaka@gmail.com>
2015-01-30 15:34:41 +0900

    [vim] add .vimrc

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ ln -sfh $DOTFILES_DIR/.zsh $HOME/.zsh
 ln -sf $DOTFILES_DIR/.gitconfig $HOME/.gitconfig
 ln -sf $DOTFILES_DIR/.tigrc $HOME/.tigrc
 ln -sfh $DOTFILES_DIR/.byobu $HOME/.byobu
+ln -sf $DOTFILES_DIR/.vimrc $HOME/.vimrc
