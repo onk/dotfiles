154d42dbcfe8a81173bf98c3a5a57e8cf2c0a863
Takafumi ONAKA <takafumi.onaka@gmail.com>
2015-02-10 05:22:40 +0900

    [git] global gitignore

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ DOTFILES_DIR=$HOME/dotfiles
 ln -sf $DOTFILES_DIR/.zshrc $HOME/.zshrc
 ln -sfh $DOTFILES_DIR/.zsh $HOME/.zsh
 ln -sf $DOTFILES_DIR/.gitconfig $HOME/.gitconfig
+ln -sf $DOTFILES_DIR/.gitignore_global $HOME/.gitignore_global
 ln -sf $DOTFILES_DIR/.tigrc $HOME/.tigrc
 ln -sfh $DOTFILES_DIR/.byobu $HOME/.byobu
 ln -sf $DOTFILES_DIR/.vimrc $HOME/.vimrc
