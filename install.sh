7ea2a1bfd3f4e7cbc85473925491d733bf5edb44
Takafumi ONAKA <takafumi.onaka@gmail.com>
2015-03-12 04:27:34 +0900

    linux 環境だと -h オプションがない

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 DOTFILES_DIR=$HOME/dotfiles
 ln -sf $DOTFILES_DIR/.zshrc $HOME/.zshrc
-ln -sfh $DOTFILES_DIR/.zsh $HOME/.zsh
+ln -sfn $DOTFILES_DIR/.zsh $HOME/.zsh
 mkdir -p $HOME/.zsh/plugins/bd
 curl https://raw.githubusercontent.com/Tarrasch/zsh-bd/master/bd.zsh > $HOME/.zsh/plugins/bd/bd.zsh
 ln -sf $DOTFILES_DIR/.gitconfig $HOME/.gitconfig
 ln -sf $DOTFILES_DIR/.gitignore_global $HOME/.gitignore_global
 ln -sf $DOTFILES_DIR/.tigrc $HOME/.tigrc
-ln -sfh $DOTFILES_DIR/.byobu $HOME/.byobu
+ln -sfn $DOTFILES_DIR/.byobu $HOME/.byobu
 ln -sf $DOTFILES_DIR/.vimrc $HOME/.vimrc
 ln -sf $DOTFILES_DIR/.gemrc $HOME/.gemrc
 ln -sf $DOTFILES_DIR/.railsrc $HOME/.railsrc
