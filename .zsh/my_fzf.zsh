06ba168063a2fa64d2c24928f30297f2b57fd1ea
Takafumi ONAKA <takafumi.onaka@gmail.com>
2025-02-22 13:44:13 +0900

    [zsh] peco -> sk --no-mouse

--- .zsh/my_fzf.zsh
+++ .zsh/my_fzf.zsh
@@ -1,6 +1,8 @@
 # fzfとghqでローカルのリポジトリクローンに飛ぶ
 function fzf-src () {
-  local selected_dir=$(ghq list --full-path | peco --prompt "PROJECT>" --query "$LBUFFER")
+  local selected_dir=$(ghq list --full-path | sk --no-mouse --prompt "PROJECT>" --query "$LBUFFER" --preview \
+    '[[ -f {}/README.md ]] && bat --style=numbers --color=always {}/README.md || echo "No README found"' --preview-window=right:40%
+  )
   if [ -n "$selected_dir" ]; then
     BUFFER="cd ${selected_dir}"
     zle accept-line
@@ -21,7 +23,7 @@ function fzf-select-history() {
   BUFFER=$(\history -n -100000 | \
     eval $tac | \
     awk '!a[$0]++' | \
-    peco --prompt "HISTORY>" --query "$LBUFFER" | gsed -e 's/\\n/\n/g' | gsed -e 's/\\t/\t/g')
+    sk --no-mouse --prompt "HISTORY>" --query "$LBUFFER" | gsed -e 's/\\n/\n/g' | gsed -e 's/\\t/\t/g')
   CURSOR=$#BUFFER
   zle clear-screen
 }
@@ -30,19 +32,19 @@ bindkey '^r' fzf-select-history
 
 # B でブランチ選択
 function fzf-git-branch() {
-  git branch -a | peco --prompt "GIT BRANCH>" | head -n 1 | sed -e "s/^\*\s*//g"
+  git branch -a | sk --no-mouse --prompt "GIT BRANCH>" | head -n 1 | sed -e "s/^\*\s*//g"
 }
 alias -g B='$(fzf-git-branch)'
 
 # F で git status からファイル選択
 function fzf-git-changed-files() {
-  git status -s | peco --prompt "multi: C-Space>" | awk '{print $2}'
+  git status -s | sk --no-mouse --prompt "multi: C-Space>" | awk '{print $2}'
 }
 alias -g F='$(fzf-git-changed-files)'
 
 # vv で git 管理下のファイルを vim で開く。更新順に並び替えしてある
 function fzf-git-ls-files() {
-  git ls-files | egrep "\.(rb|slim|haml|erb|sass|scss|coffee|js|tsx?|pm|pl|t)$"  | xargs ls -At | peco --prompt "FILES>"
+  git ls-files | egrep "\.(rb|slim|haml|erb|sass|scss|coffee|js|tsx?|pm|pl|t)$"  | xargs ls -At | sk --no-mouse --prompt "FILES>"
 }
 alias vv='vim -p $(fzf-git-ls-files)'
 
@@ -52,7 +54,7 @@ function genymotion_fzf() {
   # local つけるとエラー
   # "Google Nexus 5 - 4.4.4 - API 19 - 1080x1920"
   # genymotion_fzf:local:2: not an identifier: 5
-  vm_name=`VBoxManage list vms | peco | gsed "s/\"\(.*\)\".*/\1/"`
+  vm_name=`VBoxManage list vms | sk --no-mouse | gsed "s/\"\(.*\)\".*/\1/"`
   if [ -n "$vm_name" ]; then
     echo "boot $vm_name"
     $player --vm-name "$vm_name" &
@@ -68,7 +70,7 @@ function ec2-ssh() {
   if [ "$hosts" = "$head" ]; then
     ssh $(echo "$head" | jq -r .private_ip_address)
   else
-    host=$(echo "$hosts" | peco)
+    host=$(echo "$hosts" | sk --no-mouse)
     ssh $(echo $host | jq -r .private_ip_address)
   fi
 }
